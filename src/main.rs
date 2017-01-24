63f617c94bc66ce5afe48238b21964cc18daeab9
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-01-23 22:57:56 -0500

    make a shell that runs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub mod rsh;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut argv = env::args();
+    
+    let path = argv.
+        next().
+        unwrap_or(".".to_string());
+
+    let s = rsh::State::new(path);
+    rsh::run(s)
 }
