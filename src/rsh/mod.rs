22ccf3b3ec57cc1a9008e5f7f199a64454a19f07
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-01-23 22:58:28 -0500

    define a State struct and make a run func for teh shell

--- src/rsh/mod.rs
+++ src/rsh/mod.rs
@@ -1,9 +1,33 @@
 pub mod builtins;
-use std;
+
+use std::path::PathBuf;
+use std::io;
 
 #[derive(PartialEq, Debug)]
 pub struct State {
-    cwd: std::Path,
+    cwd: PathBuf,
+    builtins: Vec<builtins::Builtin>,
 }
 
-pub fn run(state: State)
+impl State {
+    pub fn new(cwd: String) -> State {
+        State{
+            cwd: PathBuf::from(cwd),
+            builtins: builtins::load(),
+        }
+    }
+}
+
+pub fn run(state: State) {
+    loop {
+        print!("\n{} -> ", state.cwd.to_str().unwrap_or(""));
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                println!("{:?}", state);
+                println!("{}", input);
+            },
+            Err(e) => println!("error: {}", e),
+        };
+    }
+}
