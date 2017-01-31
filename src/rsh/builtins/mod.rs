7b5bb5ea58a6dded517acc42e03bf6b0a95f2a06
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-30 20:07:26 -0500

    Create echo builtin

--- src/rsh/builtins/mod.rs
+++ src/rsh/builtins/mod.rs
@@ -6,3 +6,15 @@ pub type Builtin = fn(State) -> State;
 pub fn load() -> HashMap<String, Builtin> {
     HashMap::new()
 }
+
+pub fn echo(s: State) -> State {
+    if s.argv[1] == "-n" {
+        let strings = &s.argv[2..s.argv.len()].join(" ");
+        print!("{}", strings);
+    } else {
+        let strings = &s.argv[1..s.argv.len()].join(" ");
+        println!("{}", strings);
+    }
+
+    s
+}
