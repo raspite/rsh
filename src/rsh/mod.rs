9a1ecd53cf5d6761d48b94db322c91277d6a7646
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-31 16:25:08 +0000

    Update parse_args to handle many situations
    - empty
    - single word
    - single word enclosed in parens
    - multiple words enclosed in parens
    - multiple words enclosed in multiple sections of parens

--- src/rsh/mod.rs
+++ src/rsh/mod.rs
@@ -60,14 +60,81 @@ pub fn run(initial_state: State) {
 }
 
 fn parse_args(args: &String) -> Vec<String> {
-    let mut result: Vec<String> = vec!["test".to_string()];
+    let mut result: Vec<String> = Vec::new();
+
+    if args.len() == 0 {
+        return result
+    }
+
+    let mut building_string: bool = false;
+    let mut build_string: String = String::from("");
+    for string in args.split_whitespace() {
+        if string.starts_with("\"") {
+            if string.ends_with("\"") {
+                build_string.push_str(string);
+                result.push(build_string);
+
+                building_string = false;
+                build_string = String::from("");
+            } else {
+                building_string = true;
+
+                build_string.push_str(string);
+                build_string.push(' ');
+            }
+        } else if string.ends_with("\"") {
+            build_string.push_str(string);
+            result.push(build_string);
+
+            building_string = false;
+            build_string = String::from("");
+        } else {
+            if building_string {
+                build_string.push_str(string);
+                build_string.push(' ');
+            } else {
+                result.push(string.to_string());
+            }
+        }
+    }
+
     result
 }
 
 #[test]
 fn parse_args_test() {
-    let args = String::from("echo -n \"Hello World\"");
-    let result = parse_args(&args);
-    let expected = vec!["echo".to_string(), "-n".into(), "\"Hello World\"".into()];
-    assert_eq!(result, expected);
+    // parse empty string
+    {
+        let expected: Vec<String> = Vec::new();
+        let result = parse_args(&String::from(""));
+        assert_eq!(result, expected);
+    }
+
+    // parse single-word string
+    {
+        let expected: Vec<String> = vec!["echo".to_string()];
+        let result = parse_args(&String::from("echo"));
+        assert_eq!(result, expected);
+    }
+
+    // parse single-word string inside parens
+    {
+        let expected = vec!["\"echo\"".to_string()];
+        let result = parse_args(&String::from("\"echo\""));
+        assert_eq!(result, expected);
+    }
+
+    // parse multi-word string with closed parens section
+    {
+        let expected = vec!["echo".to_string(), "-n".into(), "\"Hello Dear World\"".into()];
+        let result = parse_args(&String::from("echo -n \"Hello Dear World\""));
+        assert_eq!(result, expected);
+    }
+
+    // parse multi-word string with multiple closed parents sections
+    {
+        let expected = vec!["echo".to_string(), "\"Hello\"".into(), "\"Dear World\"".into()];
+        let result = parse_args(&String::from("echo \"Hello\" \"Dear World\""));
+        assert_eq!(result, expected);
+    }
 }
