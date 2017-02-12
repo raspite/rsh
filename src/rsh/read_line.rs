938bb2b8478c68e9a9a1fa0a07ae29df684afcb6
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-02-12 15:41:02 -0500

    actually populate completions in input

--- src/rsh/read_line.rs
+++ src/rsh/read_line.rs
@@ -1,9 +1,11 @@
 use std::io;
 use std::io::{Write, Read};
+use std::ffi::OsStr;
 
 use radix_trie::Trie;
 
 use rsh::State;
+use rsh::builtins;
 
 pub struct Input {
     completions: Trie<String, String>,
@@ -11,7 +13,42 @@ pub struct Input {
 
 impl Input {
     pub fn from(s: &State) -> Input {
-        Input { completions: Trie::new() }
+        let mut i = Input { completions: Trie::new() };
+        let mut strs: Vec<String> = Vec::new();
+
+        // we don't have to worry about directories here anything in the
+        // path should be an executable
+        for path in s.exec_paths().iter() {
+            let items = path.iter().collect::<Vec<&OsStr>>();
+            let mut strings = items.iter()
+                .map(|x| x.to_str().unwrap_or(""))
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>();
+
+            strs.append(&mut strings);
+        }
+
+        let builtins = builtins::load();
+        let mut keys: Vec<String> = builtins.keys()
+            .map(|x| x.clone())
+            .collect();
+
+        strs.append(&mut keys);
+
+        for s in strs {
+            let mut chars = s.chars();
+            let mut current_char = chars.next();
+            let mut next_char = chars.next();
+
+            while current_char.is_some() && next_char.is_some() {
+                i.completions.insert(current_char.unwrap().to_string(),
+                                     next_char.unwrap().to_string());
+                current_char = next_char.clone();
+                next_char = chars.next();
+            }
+        }
+
+        i
     }
 
     pub fn prompt(&self, prompt: String) -> String {
